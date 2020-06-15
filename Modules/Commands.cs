6547b36ac85348e19d4fe84f079671d39d393941
Keanu <keanutimmermans8@gmail.com>
2020-06-15 11:53:59 +0200

    Added userinfo command.
    Also moved README to repo root.

--- Modules/Commands.cs
+++ Modules/Commands.cs
@@ -1,18 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace C_Sharp_Bot.Modules
 {
     public class Commands : ModuleBase<SocketCommandContext>
     {
         [Command("ping")]
+        [Summary("Plays ping pong with you.")]
         public async Task Ping()
         {
             await ReplyAsync("Pong");
         }
-    }
+
+        [Command("say")]
+        [Summary("Echoes a message.")]
+        public Task SayAsync([Remainder] [Summary("The text to echo")] string echo)
+            => ReplyAsync(echo);
+    };
+
+    public class InfoModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("userinfo")]
+        [Summary("Returns info about the current user, or the user parameter, if one passed.")]
+        [Alias("user", "whois")]
+        public async Task UserInfoAsync(
+        [Summary("The (optional) user to get info from")]
+        SocketUser user = null)
+        {
+            var userInfo = user ?? Context.Client.CurrentUser;
+            await ReplyAsync($"{userInfo.Username}#{userInfo.Discriminator}");
+        }
+    };
 }
